68a26cc07572865b18a3283e87ff9959c50059a3
CCowens93 <cassandracowens93@gmail.com>
2019-11-06 10:32:12 -0500

    initial commit

---
+++ client/build/precache-manifest.11c2fa1251ca426444a30af22327941e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b106d37634e4ba5cca9a9cc40e9aa83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a2d7f9b41ddbfb0cd5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "22a2d7f9b41ddbfb0cd5",
+    "url": "/static/js/main.28ba8a7b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
